3fda29cf5ae05debe2a4f8dbc644c13a9ecda168
Ricardo Gonzalez <rickgc@gmail.com>
2011-04-01 13:17:04 +0100

    changes overlay

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -3,19 +3,42 @@ var humanstxt = {
     // initialization code
     this.initialized = true;
     this.strings = document.getElementById("humanstxt-strings");
+    var appcontent = document.getElementById("appcontent");
+    
+    appcontent.addEventListener("pageshow", humanstxt.onPageLoad, true);
+    
+    var container = gBrowser.tabContainer;  
+    container.addEventListener("TabSelect", humanstxt.onPageLoad, false);  
+        
   },
 
-  onMenuItemCommand: function(e) {
-    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-                                  .getService(Components.interfaces.nsIPromptService);
-    promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                this.strings.getString("helloMessage"));
+  onPageLoad: function(aEvent) {
+    var feedButton = document.getElementById("humanstxt-button");
+    var feeds = gBrowser.selectedBrowser.feeds;
+    
+    feedButton.removeAttribute("feed");
+    
+    if (feeds && feeds.length > 0) {      
+      feedButton.collapsed = false;
+        
+      if (feeds.length == 1) {
+        feedButton.setAttribute("feed", feeds[0].href);
+      }  
+    } else {
+      feedButton.collapsed = true;
+    }
   },
 
-  onToolbarButtonCommand: function(e) {
-    // just reuse the function above.  you can change this, obviously!
-    humanstxt.onMenuItemCommand(e);
-  }
+  onFeedButtonClick: function(event) {
+    event.stopPropagation();
+    
+    if (event.target.hasAttribute("feed") &&
+        event.eventPhase == Event.AT_TARGET &&
+        (event.button == 0 || event.button == 1)) {
+        FeedHandler.subscribeToFeed(null, event);
+    }
+
+  },  
 };
 
 window.addEventListener("load", function () { humanstxt.onLoad(); }, false);
