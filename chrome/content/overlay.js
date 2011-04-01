d28140f1c58cd13301b65a2a33f6e33122d863c7
Ricardo Gonzalez <rickgc@gmail.com>
2011-04-01 18:17:39 +0100

    almost there

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -67,11 +67,11 @@ var humanstxt = {
     };
     req.send(null);
 
-    if (htxt.length > 0 && rps.match("text/plain")) {
-      humanstxtButton.collapsed = false;
-      humanstxtButton.setAttribute("tooltiptext", htxt);
-    } else {
+    if !(htxt.length > 0 && rps.match("text/plain")) {
       humanstxtButton.collapsed = true;
+    } else {
+      humanstxtButton.collapsed = false;
+      humanstxtButton.setAttribute("tooltiptext", htxt);    
     }
 
 
