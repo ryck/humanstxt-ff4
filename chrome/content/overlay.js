bccbea951fb62d086d3d897e57e129507691304d
Ricardo Gonzalez <rickgc@gmail.com>
2011-04-01 18:15:47 +0100

    almost there

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,3 +1,37 @@
+// parseUri 1.2.2
+// (c) Steven Levithan <stevenlevithan.com>
+// MIT License
+
+function parseUri (str) {
+  var o   = parseUri.options,
+    m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
+    uri = {},
+    i   = 14;
+
+  while (i--) uri[o.key[i]] = m[i] || "";
+
+  uri[o.q.name] = {};
+  uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+    if ($1) uri[o.q.name][$1] = $2;
+  });
+
+  return uri;
+};
+
+parseUri.options = {
+  strictMode: false,
+  key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+  q:   {
+    name:   "queryKey",
+    parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+  },
+  parser: {
+    strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+    loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)(((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+  }
+};
+
+
 var humanstxt = {
   init: function() {
     // initialization code
@@ -17,22 +51,29 @@ var humanstxt = {
 
     var uri = content.document.location;
     
+    var u = parseUri(uri), site = u.protocol + "://" + u.host;
+    if (u.port && u.port.strlen) site += ":" + u.port  
+    
+    htxt = null;
     var req = new XMLHttpRequest();
-    req.open('GET', uri + "/humans.txt", false);
+    req.open('GET', site + "/humans.txt", false);
 
     req.onreadystatechange = function () {
-      if (req.readyState == 4 && req.status == 200 && req.getResponseHeader("Content-Type") == "text/plain") {
+      if (req.readyState == 4 && req.status == 200) {
           htxt = req.responseText;
+          rps = req.getResponseHeader("Content-Type");
       }
+      
     };
     req.send(null);
 
-    if (htxt.length > 0) {
+    if (htxt.length > 0 && rps.match("text/plain")) {
       humanstxtButton.collapsed = false;
       humanstxtButton.setAttribute("tooltiptext", htxt);
     } else {
       humanstxtButton.collapsed = true;
     }
+
 
   },
 
